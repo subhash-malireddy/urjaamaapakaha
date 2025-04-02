95d0fd087ed38aa091f2f90cc316f746d7c53670
subhash_malireddy <ssm934@uowmail.edu.au>
2025-04-02 14:48:56 +1100

    setup global jest mock for prisma client using jest-mock-extended package

--- jest.setup.js
+++ jest.setup.js
@@ -1,23 +1,36 @@
 // Learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
 
 // Mock next/router
-jest.mock('next/router', () => ({
+jest.mock("next/router", () => ({
   useRouter: () => ({
     push: jest.fn(),
     replace: jest.fn(),
     prefetch: jest.fn(),
     back: jest.fn(),
-    pathname: '/',
+    pathname: "/",
     query: {},
   }),
 }));
 
 // Mock next/image
-jest.mock('next/image', () => ({
+jest.mock("next/image", () => ({
   __esModule: true,
   default: (props) => {
     // eslint-disable-next-line jsx-a11y/alt-text
     return <img {...props} />;
   },
-})); 
+}));
+
+// import { PrismaClient } from '@prisma/client'
+import { mockDeep, mockReset } from "jest-mock-extended";
+
+const prismaMock = mockDeep();
+jest.mock("@/lib/db", () => ({
+  __esModule: true,
+  db: prismaMock,
+}));
+
+beforeEach(() => {
+  mockReset(prismaMock);
+});
