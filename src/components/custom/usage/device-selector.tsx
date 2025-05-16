7cdcc7986c76a34508f52b03c5bae8cba6757509
subhash-malireddy <subhashmalireddy@gmail.com>
2025-05-16 13:34:10 +1000

    Implement DeviceSelector and enhance FiltersForm for device selection
    - Added DeviceSelector component to allow users to select devices from a dropdown.
    - Integrated DeviceSelector into FiltersForm, including state management for selected device.
    - Updated layout and added labels for better user experience.
    - Display selected device alias in the form for clarity.

--- src/components/custom/usage/device-selector.tsx
+++ src/components/custom/usage/device-selector.tsx
@@ -1,9 +1,41 @@
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { type DeviceSelectionList } from "@/lib/zod/usage";
 
 export default function DeviceSelector({
   devices,
+  onSelect,
+  selectedDeviceValue,
 }: {
   devices: DeviceSelectionList;
+  onSelect: (deviceId: string) => void;
+  selectedDeviceValue: string;
 }) {
-  return <div>{JSON.stringify(devices)}</div>;
+  return (
+    <Select
+      onValueChange={onSelect}
+      value={selectedDeviceValue}
+      name="deviceId"
+    >
+      <SelectTrigger className="w-[150px] md:w-[200px]">
+        <SelectValue placeholder="Select a device" />
+      </SelectTrigger>
+      <SelectContent position="popper">
+        <SelectGroup>
+          <SelectItem value={"All"}>All</SelectItem>
+          {devices.map((device) => (
+            <SelectItem key={device.id} value={device.id}>
+              {device.alias}
+            </SelectItem>
+          ))}
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  );
 }
