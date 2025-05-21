6afd8de5ef30893d06becd32cd3b79b20e760699
subhash-malireddy <subhashmalireddy@gmail.com>
2025-05-21 15:09:36 +1000

    Move the state and data fetching logic from filters-form component to chart-with-filters cmponent.

--- src/components/custom/usage/chart-with-filters.tsx
+++ src/components/custom/usage/chart-with-filters.tsx
@@ -1,8 +1,10 @@
 "use client";
 import { type DeviceSelectionList } from "@/lib/zod/usage";
 import FiltersForm from "./filters-form";
-import { useState } from "react";
+import { useEffect, useState, useTransition } from "react";
 import { getUsageDataAction } from "@/lib/actions/usage-actions";
+import { getDateRangeForTimePeriod, type TimePeriod } from "@/lib/usage-utils";
+
 export default function ChartWithFilters({
   devices,
 }: {
@@ -11,13 +13,70 @@ export default function ChartWithFilters({
   const [usageData, setUsageData] =
     useState<Awaited<ReturnType<typeof getUsageDataAction>>>();
   console.log("📜usageData:: ", usageData);
+  const [selectedDeviceId, setSelectedDeviceId] = useState<string | null>(null);
+  const [selectedTimePeriod, setSelectedTimePeriod] =
+    useState<TimePeriod>("current week");
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [isPending, startTransition] = useTransition();
+
+  const handleDeviceSelect = (deviceId: string) => {
+    if (deviceId === "All") {
+      setSelectedDeviceId(null);
+    } else {
+      setSelectedDeviceId(deviceId);
+    }
+  };
+
+  const handleTimePeriodSelect = (period: TimePeriod) => {
+    setSelectedTimePeriod(period);
+  };
+
+  const dateRange = getDateRangeForTimePeriod(selectedTimePeriod);
+
+  const selectedDeviceValue = selectedDeviceId ?? "All";
+  const selectedDeviceAlias =
+    devices.find((device) => device.id === selectedDeviceId)?.alias || "All";
+
+  useEffect(() => {
+    const fetchUsageData = (
+      deviceId: string | null,
+      timePeriod: TimePeriod,
+    ) => {
+      startTransition(async () => {
+        const result = await getUsageDataAction(
+          timePeriod,
+          dateRange,
+          deviceId || undefined,
+        );
+        setUsageData(result);
+      });
+    };
+    fetchUsageData(selectedDeviceId, selectedTimePeriod);
+  }, [selectedDeviceId, selectedTimePeriod]);
+
   return (
     <div className="flex w-full flex-col">
       <FiltersForm
         devices={devices}
-        onDataFetched={(data) => setUsageData(data)}
+        selectedDeviceValue={selectedDeviceValue}
+        selectedDeviceAlias={selectedDeviceAlias}
+        selectedTimePeriod={selectedTimePeriod}
+        handleDeviceSelect={handleDeviceSelect}
+        handleTimePeriodSelect={handleTimePeriodSelect}
       />
-      <div data-testid="chart" className="flex w-full">
+      <div data-testid="chart" className="flex w-full flex-col gap-2">
+        <p className="text-center text-lg">
+          Showing usage for&nbsp;
+          <em>
+            {selectedDeviceAlias === "All"
+              ? "All Devices"
+              : selectedDeviceAlias}
+          </em>
+          &nbsp;from&nbsp;
+          <em>{dateRange.formatted.start}</em>
+          &nbsp;to&nbsp;
+          <em>{dateRange.formatted.end}</em>
+        </p>
         {/* for now, we will display textual data as per the details in usage-screen-implemenation notepad */}
         {usageData && (
           <div className="flex flex-col gap-2">
--- src/components/custom/usage/filters-form.tsx
+++ src/components/custom/usage/filters-form.tsx
@@ -4,66 +4,24 @@ import { type DeviceSelectionList } from "@/lib/zod/usage";
 import TimePeriodSelector from "./time-period-selector";
 import DeviceSelector from "./device-selector";
 import { Label } from "@/components/ui/label";
-import { useEffect, useState, useTransition } from "react";
-import { getDateRangeForTimePeriod, type TimePeriod } from "@/lib/usage-utils";
-import { getUsageDataAction } from "@/lib/actions/usage-actions";
-
+import { type TimePeriod } from "@/lib/usage-utils";
 interface FiltersFormProps {
   devices: DeviceSelectionList;
-  onDataFetched: (data: Awaited<ReturnType<typeof getUsageDataAction>>) => void;
+  selectedDeviceValue: string;
+  selectedDeviceAlias: string;
+  selectedTimePeriod: TimePeriod;
+  handleDeviceSelect: (deviceId: string) => void;
+  handleTimePeriodSelect: (timePeriod: TimePeriod) => void;
 }
 
 export default function FiltersForm({
   devices,
-  onDataFetched,
+  selectedDeviceValue,
+  selectedDeviceAlias,
+  selectedTimePeriod,
+  handleDeviceSelect,
+  handleTimePeriodSelect,
 }: FiltersFormProps) {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [isPending, startTransition] = useTransition();
-  const [selectedDeviceId, setSelectedDeviceId] = useState<string | null>(null);
-  const [selectedTimePeriod, setSelectedTimePeriod] =
-    useState<TimePeriod>("current week");
-  // const [usageData, setUsageData] =
-  //   useState<Awaited<ReturnType<typeof getUsageDataAction>>>();
-
-  // console.log("📜usageData:: ", usageData);
-  const handleDeviceSelect = (deviceId: string) => {
-    if (deviceId === "All") {
-      setSelectedDeviceId(null);
-    } else {
-      setSelectedDeviceId(deviceId);
-    }
-    // fetchUsageData(deviceId === "All" ? null : deviceId, selectedTimePeriod);
-  };
-
-  const handleTimePeriodSelect = (period: TimePeriod) => {
-    setSelectedTimePeriod(period);
-    // fetchUsageData(selectedDeviceId, period);
-  };
-
-  const dateRange = getDateRangeForTimePeriod(selectedTimePeriod);
-
-  const selectedDeviceValue = selectedDeviceId ?? "All";
-  const selectedDeviceAlias =
-    devices.find((device) => device.id === selectedDeviceId)?.alias || "All";
-
-  useEffect(() => {
-    console.log("initializing usage data");
-    const fetchUsageData = (
-      deviceId: string | null,
-      timePeriod: TimePeriod,
-    ) => {
-      startTransition(async () => {
-        const result = await getUsageDataAction(
-          timePeriod,
-          dateRange,
-          deviceId || undefined,
-        );
-        onDataFetched(result);
-      });
-    };
-    fetchUsageData(selectedDeviceId, selectedTimePeriod);
-  }, [selectedDeviceId, selectedTimePeriod]);
-
   return (
     <div data-testid="filters-form" className="flex w-full flex-col gap-2">
       <form className="flex w-full justify-between">
@@ -88,16 +46,6 @@ export default function FiltersForm({
           />
         </div>
       </form>
-      <p className="text-center text-lg">
-        Showing usage for&nbsp;
-        <em>
-          {selectedDeviceAlias === "All" ? "All Devices" : selectedDeviceAlias}
-        </em>
-        &nbsp;from&nbsp;
-        <em>{dateRange.formatted.start}</em>
-        &nbsp;to&nbsp;
-        <em>{dateRange.formatted.end}</em>
-      </p>
     </div>
   );
 }
